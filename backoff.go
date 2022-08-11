9da8ce9c4f9d812240517662a53357cdc2e6d4cc
Till Knuesting <t.knuesting@sap.com>
2022-08-11 21:21:05 +0100

    backoff retry with nondeter jitter

--- backoff.go
+++ backoff.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	crand "crypto/rand"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"log"
@@ -9,7 +11,12 @@ import (
 )
 
 func main() {
-	fmt.Println(withExponentialBackoffAndJitter())
+	s, err := retryWithExponentialBackoffAndJitter(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(s)
 }
 
 var count = 0
@@ -17,31 +24,62 @@ var count = 0
 func sendRequest() (string, error) {
 	count++
 
-	if count <= 3 {
-		return "intentional fail", errors.New("error")
-	} else {
-		return "success", nil
+	if count <= 50 {
+		return "", errors.New("error from send request")
 	}
+
+	return "success", nil
+}
+
+type cryptoSource struct{}
+
+func (s cryptoSource) Seed(seed int64) {}
+
+func (s cryptoSource) Int63() int64 {
+	return int64(s.Uint64() & ^uint64(1<<63))
+}
+
+func (s cryptoSource) Uint64() (v uint64) {
+	err := binary.Read(crand.Reader, binary.BigEndian, &v)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return v
 }
 
 // Using withExponentialBackoff as the backoff function sends retries
 // initially with a 1-second delay, but doubling after each attempt to
 // a maximum delay of 1-minute. Each backoff time gets an additional
-func withExponentialBackoffAndJitter() string {
+// randomness is generated cryptographic secure and nondeterministic.
+func retryWithExponentialBackoffAndJitter(retries int) (string, error) {
 	res, err := sendRequest()
-	base, cap := time.Second, time.Minute
+
+	base, caP := time.Second, time.Minute
+
+	var src cryptoSource
+	rnd := rand.New(src)
+
+	var retryCounter int
 
 	for backoff := base; err != nil; backoff <<= 1 {
-		if backoff > cap {
-			backoff = cap
+		if retryCounter >= retries {
+			return res, fmt.Errorf("maximum retries reached: %w", err)
 		}
 
-		jitter := rand.Int63n(int64(backoff * 3))
+		if backoff > caP {
+			backoff = caP
+		}
+
+		jitter := rnd.Int63n(int64(backoff * 3))
 		sleep := base + time.Duration(jitter)
 		log.Println("Error while sending request, retrying in", sleep)
 		time.Sleep(sleep)
+
 		res, err = sendRequest()
+
+		retryCounter++
 	}
 
-	return res
+	return res, nil
 }
